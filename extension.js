30d6fe2a7a6c550ac8d2b4788990af5510d8e8f0
Markerto <erak61@protonmail.com>
2025-10-12 09:47:36 +0300

    Update extension.js (v2.1)

--- extension.js
+++ extension.js
@@ -1,42 +1,84 @@
-//gnome 45+ code
+//for gnome 48
 import Clutter from 'gi://Clutter';
 import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
 import St from 'gi://St';
-
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 export default class ShowDesktopClickExtension {
     constructor() {
         this._windowsHidden = false;
         this._hiddenWindows = [];
+        this._hiddenActors = [];
         this._captureEventId = null;
+        this._windowFocusIds = [];
     }
-
+    
+    _restoreAllWindows() {
+        // Show windows
+        this._hiddenWindows.forEach(win => {
+            win.unminimize();
+        });
+        this._hiddenWindows = [];
+        
+        // For Modal windows
+        this._hiddenActors.forEach(actor => {
+            actor.show();
+        });
+        this._hiddenActors = [];
+        
+        // Disconnect window focus handlers
+        this._windowFocusIds.forEach(({win, id}) => {
+            win.disconnect(id);
+        });
+        this._windowFocusIds = [];
+        
+        this._windowsHidden = false;
+    }
+    
     _toggleDesktop() {
         const workspace = global.workspace_manager.get_active_workspace();
         const windows = workspace.list_windows();
         
         if (this._windowsHidden) {
-            // Show windows
-            this._hiddenWindows.forEach(win => {
-                win.unminimize();
-            });
-            this._hiddenWindows = [];
-            this._windowsHidden = false;
+            this._restoreAllWindows();
         } else {
             // Hide windows
             this._hiddenWindows = [];
+            this._hiddenActors = [];
+            this._windowFocusIds = [];
+            
             windows.forEach(win => {
-                if (!win.minimized && win.get_window_type() === Meta.WindowType.NORMAL) {
-                    this._hiddenWindows.push(win);
-                    win.minimize();
+                if (!win.minimized) {
+                    const windowType = win.get_window_type();
+                    
+                    if (windowType === Meta.WindowType.NORMAL) {
+                        // Minimize normal windows
+                        this._hiddenWindows.push(win);
+                        win.minimize();
+                    } else if (windowType === Meta.WindowType.MODAL_DIALOG || 
+                               windowType === Meta.WindowType.DIALOG) {
+                        // Hide modal/dialog windows
+                        const actor = win.get_compositor_private();
+                        if (actor && actor.visible) {
+                            this._hiddenActors.push(actor);
+                            actor.hide();
+                            
+                            // Connect to focus-in event to restore all when modal is activated
+                            const focusId = win.connect('focus', () => {
+                                if (this._windowsHidden) {
+                                    this._restoreAllWindows();
+                                }
+                            });
+                            this._windowFocusIds.push({win: win, id: focusId});
+                        }
+                    }
                 }
             });
             this._windowsHidden = true;
         }
     }
-
+    
     _onBackgroundClick(actor, event) {
         // Only respond to primary button clicks
         if (event.get_button() !== 1) {
@@ -65,7 +107,7 @@ export default class ShowDesktopClickExtension {
         
         return Clutter.EVENT_PROPAGATE;
     }
-
+    
     enable() {
         // Connect to capture event on the stage
         this._captureEventId = global.stage.connect('captured-event', (actor, event) => {
@@ -75,7 +117,7 @@ export default class ShowDesktopClickExtension {
             return Clutter.EVENT_PROPAGATE;
         });
     }
-
+    
     disable() {
         // Disconnect event handler
         if (this._captureEventId) {
@@ -85,11 +127,7 @@ export default class ShowDesktopClickExtension {
         
         // restore hidden windows
         if (this._windowsHidden) {
-            this._hiddenWindows.forEach(win => {
-                win.unminimize();
-            });
-            this._hiddenWindows = [];
-            this._windowsHidden = false;
+            this._restoreAllWindows();
         }
     }
 }
